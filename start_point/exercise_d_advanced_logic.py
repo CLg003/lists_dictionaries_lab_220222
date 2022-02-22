726acb320f38bef4973e756fd4f6646fbeea6a44
CLg003 <clairemjlaing@gmail.com>
2022-02-22 17:30:47 +0000

    final commit, cannot finish exercise d question 4

--- start_point/exercise_d_advanced_logic.py
+++ start_point/exercise_d_advanced_logic.py
@@ -15,14 +15,24 @@ for num in numbers:
 # print(numbers[-1] - numbers[0])
 
 # 3. Print True if the list contains a 2 next to a 2 somewhere.
-for index in range(0, (len(numbers) - 1)):
-    if (numbers[index] == 2) and (numbers[index + 1] == 2):
-        print(True)
+# for index in range(0, (len(numbers) - 1)):
+#     if (numbers[index] == 2) and (numbers[index + 1] == 2):
+#         print(True)
 
 # 4. Print the sum of the numbers, 
 #    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
 #    
 #    So [11, 6, 4, 99, 7, 11] would have sum of 22
+six_index = []
+seven_index = []
+for index in range(0, len(numbers)):
+    if numbers[index] == 6:
+        six_index.append(index)
+    elif numbers[index] == 7:
+        seven_index.append(index)
+print(six_index)
+print(seven_index)
+updated_numbers = []
 
 
 # 5. HARD! Print the sum of the numbers. 
@@ -31,6 +41,11 @@ for index in range(0, (len(numbers) - 1)):
 #    HINT - You will need to track the index throughout the loop.
 #
 #    So [5, 13, 2] would have sum of 5. 
+sum = 0
+for index in range(0, len(numbers)):
+    if (numbers[index] != 13) and (numbers[index - 1] != 13):
+        sum += numbers[index]
+# print(sum)
 
 
 
